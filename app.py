edeeb1803c25d464d5b046c45f566f14828271e1
parmeshwar909736-sudo <parmeshwar909736@gmail.com>
2026-06-25 22:01:01 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def home():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
